e4a695e4759c94486b0e8720acfb8d67872ae4e1
RichardDannie <60300672+RichardDannie@users.noreply.github.com>
2020-02-16 16:46:33 -0500

    responsive update

---
+++ script.js
@@ -0,0 +1,9 @@
+function menuButton() {
+  var x = document.getElementsByClassName("nav_ul")[0];
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  	}
+  	else {
+    x.style.display = "block";
+  	}
+}
