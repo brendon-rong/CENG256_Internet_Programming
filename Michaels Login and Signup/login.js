e0d25814ba8198b7de823f7de2aa2f11a340144b
mikietonon <mikietonon@live.com>
2020-02-18 17:56:29 -0500

    ghf

---
+++ Michaels Login and Signup/login.js
@@ -0,0 +1,49 @@
+$(document).ready(init);
+
+function init(){
+	$(".error").hide();
+	$("#submit_btn").click(validate);
+}
+function validate(event){
+	$(".error").hide();
+
+	var password = $("input#password").val();
+	var email = $("input#email").val();
+
+	if (email == ""){
+		$("label#email_error").show();
+		$("input#email").focus();
+		return false;
+	}
+
+	if (password == ""){
+		$("label#password_error").show();
+		$("input#password").focus();
+		return false;
+	}
+
+	var dataString = 'name='+ name + '&email=' + email + '&phone=' + phone;
+
+	return false;
+}
+
+  $(function() {
+    $('.error').hide();
+    $(".button").click(function() {
+      
+      $('.error').hide();
+
+      var email = $("input#email").val();
+      if (email == "") {
+        $("label#email_error").show();
+        $("input#email").focus();
+      }
+
+      var password = $("input#password").val();
+      if (password == "") {
+        $("label#password_error").show();
+        $("input#password").focus();
+        return false;
+      }
+    });
+  });
