3092c33c3e50c0c948ce57b4e3e65b8a8cddaeac
Richard Dannie <Richard.Dannie@users.noreply.github.com>
2020-02-20 16:06:18 -0500

    homepage done

--- home_page.js
+++ home_page.js
@@ -17,3 +17,24 @@ function showNav() {
 		list.li.style.display = "inline-block";
 	}
 }
+
+//homepage functions
+function displayNextImage() {
+	x = (x === images.length - 1) ? 0 : x + 1;
+	document.getElementById("homepage_img").src = images[x];
+	}
+
+	function displayPreviousImage() {
+	x = (x <= 0) ? images.length - 1 : x - 1;
+	document.getElementById("homepage_img").src = images[x];
+	}
+
+	function startTimer() {
+	setInterval(displayNextImage, 3000);
+	}
+
+	var images = [], x = -1;
+	images[0] = "images/image1.png";
+	images[1] = "images/image2.png";
+	images[2] = "images/image3.png";
+	images[3] = "images/image4.png";
